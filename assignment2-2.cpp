4dec3bd6c6df7d57830078b1cb01dee24bcbde9b
Joshua Ko <kojosh07@gmail.com>
2022-09-01 18:59:12 -0700

    Assignment 2-2

---
+++ assignment2-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+  
+int main() {
+  char c;
+	c = 65;		// ASCII Value for "A"
+	c = c + 'a' - 'A';
+  cout << c << endl;
+	c = 66;		// ASCII Value for "B" 
+  cout << c << endl;
+	
+}
